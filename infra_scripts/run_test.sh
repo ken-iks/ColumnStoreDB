a3071688cd556cfb90486475270c912390da6fc8
Sanket Jayant Purandare <sanketpurandare@fb.com>
2023-11-27 13:07:26 -0500

    Fixing minor bugs in run_test.sh

--- infra_scripts/run_test.sh
+++ infra_scripts/run_test.sh
@@ -41,8 +41,8 @@ then
     echo Test $test_id is a performance test. Checking its speedup...
     prev_time=`cat tmp.prev_time`
     curr_time=`expr $end - $start`
-    speedup=`printf "%.4f\n" $((10**4 * curr_time/prev_time))e-4`
-    ths=2.0
+    speedup=`printf "%.4f\n" $((10**4 * prev_time/curr_time))e-4`
+    ths=1.5
     echo Speedup is $speedup. Checking whether it satisfies the performance requirements for threshold of $ths\...
     awk -v n1="$speedup" -v n2="$ths" 'BEGIN { printf "" (n1>=n2?  "Yes, it does. Success! [\033[42mok\033[0m]\n"  : "No, it does not. Failure! [\033[31mfail\033[0m]\n")}'
     rm tmp.prev_time
@@ -55,7 +55,7 @@ then
     echo Test $test_id is a performance test. Checking its speedup...
     prev_time=`cat tmp.prev_time`
     curr_time=`expr $end - $start`
-    speedup=`printf "%.4f\n" $((10**4 * curr_time/prev_time))e-4`
+    speedup=`printf "%.4f\n" $((10**4 * prev_time/curr_time))e-4`
     ths=1.01
     echo Speedup is $speedup. Checking whether it satisfies the performance requirements for threshold of $ths\...
     awk -v n1="$speedup" -v n2="$ths" 'BEGIN { printf "" (n1>=n2?  "Yes, it does. Success! [\033[42mok\033[0m]\n"  : "No, it does not. Failure! [\033[31mfail\033[0m]\n")}'
@@ -64,7 +64,7 @@ then
     echo Now, checking whether it also outputs correct results...
 fi
 
-if [[ $test_id == 16 ]] || [[ $test_id == 18 ]] || [[ $test_id == 33 ]] [[ $test_id == 34 ]] || [[ $test_id == 36 ]] || [[ $test_id == 37 ]] || [[ $test_id == 39 ]] || [[ $test_id == 40 ]] || [[ $test_id == 42 ]] || [[ $test_id == 43 ]]
+if [[ $test_id == 16 ]] || [[ $test_id == 18 ]] || [[ $test_id == 33 ]] || [[ $test_id == 34 ]] || [[ $test_id == 36 ]] || [[ $test_id == 37 ]] || [[ $test_id == 39 ]] || [[ $test_id == 40 ]] || [[ $test_id == 42 ]] || [[ $test_id == 43 ]]
 then
     prev_time=`expr $end - $start`
     echo $prev_time > tmp.prev_time
